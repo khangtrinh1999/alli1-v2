1898a8b04b589950b48045f881ce22c7b13f2d32
khangtrinh1999 <khanggran3147@gmail.com>
2025-05-03 21:14:16 +1000

    app route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+// app/page.tsx
+
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div></div>
-  );
+  redirect('/dashboard');
 }
--- components/ui/sidebar/sidebar-menu-item.tsx
+++ components/ui/sidebar/sidebar-menu-item.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 interface SideBarMenuItemProps {
